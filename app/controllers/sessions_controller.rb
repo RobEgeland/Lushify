0e7e85ca2ece2afc89e4ac39c52d6fd2a2b2625b
Robert Egeland <robegeland@yahoo.com>
2023-03-13 20:12:01 -0500

    adds user & sessions controler

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,14 @@
+class SessionsController < ApplicationController
+    def create 
+        user = User.find_by(name: params[:name])
+        if user&.authenticate(params[:password])
+            session[:user_id] = user.id
+            render json: user, status: :ok
+        else 
+            render json: {errors: "Invalid username or password"}, status: :unauthorized
+        end
+    def destroy
+        session.delete :user_id 
+        head :no_content
+    end
+end
