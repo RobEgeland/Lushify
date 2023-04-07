3c340d2d33f45ee113a0b94b2a6066ae11186191
Robert Egeland <robegeland@yahoo.com>
2023-04-06 19:50:05 -0500

    adds order-type/employe input to take order

--- client/src/Components/TakeOrder.js
+++ client/src/Components/TakeOrder.js
@@ -2,7 +2,25 @@ import React from 'react'
 
 const TakeOrder = () => {
   return (
-    <div>TakeOrder</div>
+    <div>
+      <div className='parent'>
+        <div className='input'>
+        <input type="text" autocomplete="on" name="text" class="input" placeholder="Employee"/>
+        </div>
+        <div className='radio-parent'> 
+          <div class="radio-inputs">
+            <label class="radio">
+              <input type="radio" name="radio" checked/>
+              <span class="name">Delivery</span>
+            </label>
+            <label class="radio">
+              <input type="radio" name="radio"/>
+              <span class="name">Pickup</span>
+            </label>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
