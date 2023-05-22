240b41ac7f21592b7aed47bb101563048ff59a58
Rob Egeland <robegeland@yahoo.com>
2023-05-22 17:50:48 -0500

    add city state and potal code to users

--- db/migrate/20230314005912_create_users.rb
+++ db/migrate/20230314005912_create_users.rb
@@ -1,9 +1,12 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :name
+      t.string :business_name
       t.string :email
       t.string :password_digest
+      t.string :city
+      t.string :state
+      t.integer :postal_code
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -15,9 +15,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_14_005912) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "business_name"
     t.string "email"
     t.string "password_digest"
+    t.string "city"
+    t.string "state"
+    t.integer "postal_code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
